0ab6f614af266f66bca95a6377ed95c19f9f920a
DK\ALTU <altu@sitecore.net>
2018-06-14 17:11:51 +0300

    original code

---
+++ src/Sitecore.Support.193758.197257/ContentEditorForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Web.UI.HtmlControls;
+using Sitecore.Web.UI.Sheer;
+using System.Collections;
+using Sitecore.Shell.Applications.ContentManager;
+
+namespace Sitecore.Support.Shell.Applications.ContentManager
+{
+  public class ContentEditorForm : Sitecore.Shell.Applications.ContentManager.ContentEditorForm
+  {
+    public new void TreeSearchOptionName_Click()
+    {
+      string control = Context.ClientPage.ClientRequest.Control;
+      string text = StringUtil.Mid(control, 20);
+      SheerResponse.DisableOutput();
+      Sitecore.Web.UI.HtmlControls.Menu menu = new Sitecore.Web.UI.HtmlControls.Menu();
+      bool flag = false;
+      SortedList sortedList = new SortedList(StringComparer.Ordinal);
+      foreach (FieldInfo fieldInfo in base.FieldInfo.Values)
+      {
+        Item item = Client.ContentDatabase.GetItem(fieldInfo.FieldID);
+        if (item != null)
+        {
+          if (!sortedList.ContainsKey(item.GetUIDisplayName()))
+          {
+            sortedList.Add(item.GetUIDisplayName(), item.Key);
+          }
+          flag = true;
+        }
+      }
+      if (flag)
+      {
+        foreach (string text2 in sortedList.Keys)
+        {
+          menu.Add("F" + sortedList[text2], text2, string.Empty, string.Empty, string.Format("javascript:scContent.changeSearchCriteria(\"{0}\",\"{1}\",\"{2}\")", text, text2, text2), false, string.Empty, MenuItemType.Normal);
+        }
+        menu.AddDivider();
+      }
+      menu.Add("Remove", "Remove", string.Empty, string.Empty, "javascript:scContent.removeSearchCriteria(\"" + text + "\")", false, string.Empty, MenuItemType.Normal);
+      SheerResponse.EnableOutput();
+      SheerResponse.ShowPopup(control, "below", menu);
+    }
+  }
+}
---
+++ src/Sitecore.Support.193758.197257/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.193758.197257")]
+[assembly: AssemblyProduct("Sitecore.Support.193758.197257")]
+[assembly: ComVisible(false)]
